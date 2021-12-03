c7ff73aa90f851632f4481e8b28a46d2e08f0545
jlombard <j.lombard703@laposte.net>
2021-12-03 16:43:49 +0100

    Modèle SIS local avec résolution numérique & graphe

--- main.py
+++ main.py
@@ -40,20 +40,34 @@ plt.legend()
 ############################ MODELE VERSION ILES INFINIES (Fonctionnel)#################################################
 ########################################################################################################################
 
-Nb_sites = 10
-tsim = 100
-t_petit = np.linspace(0,0.1,2) # On prends un point sur un pas très petit pour une résolution pas à pas
+Nb_sites = 2
+tsim = 10
+t_petit = np.linspace(0,1,2) # On prends un point sur un pas très petit pour une résolution pas à pas
 rho = 0.1 # Dispersal cost, utile car paramètres métapop exclusif
+epsilon = 0.1 # Extinction probability, utile car exclusif metapop
+
+## Initialisation pour le recueil des sorties
+## On crée une liste qui contient une armada de listes qui vont contenir la dynamique de chaque site
+Metapop_dyn = []
+for i in range(Nb_sites) :
+    innerlist = []
+    Metapop_dyn.append(innerlist)
+
 
 # On initialise une pop par site, dont une contient un infecté
 Pops = [] # Liste de tuples, un tuple = une pop et contient les valeurs d'effectif
-for i in range(Nb_sites-1) :
+for i in range(Nb_sites) : # On initialise la metapop de travail et la liste des sorties
     Pops.append(Init)
-Pops.append((100,1,0,0))
-
+    Metapop_dyn[i].append((100,0))
+    if i == Nb_sites-1 :
+        Pops.append((100,1,0,0))
+        Metapop_dyn[i].append((100, 1))
+print(Metapop_dyn)
+print(Pops)
 #On décrit le modèle
 for t in range(tsim):  # A chaque tour du modèle
-    print('Metapopulation',Pops)
+
+    #print('Metapopulation',Pops) # Ce print est très pratique, gardons le !
 
     Migrant_S = []  # Initialisation des listes qui contiendront les migrants de chaque site
     Migrant_I = []
@@ -68,8 +82,8 @@ for t in range(tsim):  # A chaque tour du modèle
         new_pop = (dynamics[1][0],dynamics[1][1], 0, 0) # On introduit les nouvelles valeurs de S et I
         Pops[index] = new_pop # Et on met a jour la metapop
     #Quand on est passé sur toutes les pops, on calcule le nombre total de migrants
-    Nb_mig_s_perpatch = sum(Migrant_S) / Nb_sites # Puis on divise par le nombre de patch pour avoir la qtité reçue pa chacun
-    Nb_mig_i_perpatch = sum(Migrant_I) / Nb_sites
+    Nb_mig_s_perpatch = sum(Migrant_S) *(1-rho) / Nb_sites # Puis on divise par le nombre de patch pour avoir la qtité reçue pa chacun
+    Nb_mig_i_perpatch = sum(Migrant_I) *(1-rho) / Nb_sites
     for index, pop in enumerate(Pops): # Boucle de Redistribution des migrants
 
         new_pop = (pop[0]
@@ -77,3 +91,18 @@ for t in range(tsim):  # A chaque tour du modèle
         final_pop = (new_pop[0] + new_pop[2], new_pop[1]+ new_pop[3], 0, 0 ) # On range les immigrants dans S, I et on repart avec 0 migrants "non assignés"
         Pops[index] = final_pop # Mise à jour de la metapop
 
+        #On détermine ensuite si le site s'éteint
+        p = np.random.uniform()
+        if p < epsilon :
+            Extinct_pop = (0, 0, 0, 0) # Tout le monde meurt
+            Pops[index] = Extinct_pop # Et on met à jour la metapop
+        else : pass #Sinon on continue tel quel
+        print('pop index', pop[0])
+        for index, list_pop in enumerate(Metapop_dyn) : # Mise a jour des listes de sortie
+            Effectifs = (pop[0], pop[1])
+            Metapop_dyn[index].append(Effectifs)
+    print('Sorties', Metapop_dyn)
+    # Implémenté ainsi, toute la dynamique est déterministe à l'exception des extinctions qui sont stochastiques... pas ouf (Voir Gillespie)
+    # D'autant que ca rend les résultats très dépendants du pas de mesure utilisé...
+
+# Mise en forme des sorties pour tracé de figures
